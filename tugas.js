d97df48916cad558014cd26527e9244fd0e1bdf4
Panji28 <panjiprawirowibowo@gmail.com>
2019-08-06 09:32:08 +0700

    Tugas 2 javascript
    Tugas 2 js

---
+++ tugas.js
@@ -0,0 +1,21 @@
+var nomor_telp = 02130026776; //number
+var nama = "Nama "; //string
+var hasil = "true";  //boolean
+var keliling = 190;     
+if(keliling > 180)
+keliling = "true";
+else
+keliling = "false";
+
+var kotak = null; //null
+var tidak_ada; //undefined
+var no_rumah = [20,21,22,23,24,25]; //array
+
+console.log(nomor_telp);
+console.log(nama);
+console.log(keliling);
+console.log(kotak);
+console.log(tidak_ada);
+console.log(no_rumah);
+
+
